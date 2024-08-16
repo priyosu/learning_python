935d66888623553d1b59c951aa3eb3f684b6b567
priyosu <suxpriyo@gmail.com>
2024-08-16 16:05:05 +0600

    added another function

--- function/function.py
+++ function/function.py
@@ -3,3 +3,7 @@ def calc_sum(a,b): # parameters
     return a + b
 sum = calc_sum(1,2) #function call; arguments
 print(sum)
+# Another simple function
+def print_this():
+    print("Hello World")
+print_this()
